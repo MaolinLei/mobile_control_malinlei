cf36dcec5f8d2981d2ba9e5af1c23539015fe9b8
MaolinLei <98107971+MaolinLei@users.noreply.github.com>
2025-07-22 18:32:17 +0800

    Update aruco_frame.py

--- mobile_control/aruco_frame.py
+++ mobile_control/aruco_frame.py
@@ -19,17 +19,13 @@ class ManualFramePublisher:
         # First frame: custom_frame relative to base_link
         t1 = TransformStamped()
         t1.header.stamp = rospy.Time.now()
-        t1.header.frame_id = "base_link"
-        t1.child_frame_id = "aruco_frame_first"
+        t1.header.frame_id = "mobile_base"
+        t1.child_frame_id = "second_desire"
 
-        t1.transform.translation.x = -0.043
-        t1.transform.translation.y = 0.106
-        t1.transform.translation.z = -0.155
+        t1.transform.translation.x = 0.82389
+        t1.transform.translation.y = -0.13365
+        t1.transform.translation.z = 0.135
 
-        t1.transform.rotation.x = 0.446349
-        t1.transform.rotation.y = -0.493811
-        t1.transform.rotation.z = -0.520067
-        t1.transform.rotation.w = 0.535214
 
         t1.transform.rotation.x = 0.5
         t1.transform.rotation.y = -0.5
@@ -39,35 +35,32 @@ class ManualFramePublisher:
      #    # Second frame: sub_frame relative to custom_frame
         t2 = TransformStamped()
         t2.header.stamp = t1.header.stamp
-        t2.header.frame_id = "aruco_frame_first"
-        t2.child_frame_id = "aruco_frame_second"
+        t2.header.frame_id = "mobile_base"
+        t2.child_frame_id = "first_desire"
 
-        t2.transform.translation.x = -1.17
-        t2.transform.translation.y = 0.0
-        t2.transform.translation.z = -0.11
+        t2.transform.translation.x = 0.8768
+        t2.transform.translation.y = -1.01630
+        t2.transform.translation.z = 1.35
 
-        quat = tf.transformations.quaternion_from_euler(math.pi/2, 0,  -math.pi/2)
-        t2.transform.rotation.x = 0
-        t2.transform.rotation.y =  -0.7071068
-        t2.transform.rotation.z =  0
-        t2.transform.rotation.w = 0.7071068
+        t2.transform.rotation.x = -0.5
+        t2.transform.rotation.y = -0.5
+        t2.transform.rotation.z = -0.5
+        t2.transform.rotation.w = -0.5
 
 
         t3 = TransformStamped()
         t3.header.stamp = t1.header.stamp
-        t3.header.frame_id = "aruco_frame_first"
-        t3.child_frame_id = "aruco_frame_third"
+        t3.header.frame_id = "mobile_base"
+        t3.child_frame_id = "third_desire"
 
-        t3.transform.translation.x = -1.5
-        t3.transform.translation.y = 0.0
-        t3.transform.translation.z = -0.11
-        t3.transform.rotation.x = 0
-        t3.transform.rotation.y =  0.258819
-        t3.transform.rotation.z =  0
-        t3.transform.rotation.w = 0.9659258
+        t3.transform.translation.x = 0.6825
+        t3.transform.translation.y = 1.0
+        t3.transform.translation.z = 0.135
 
-
-
+        t3.transform.rotation.x =  0.242
+        t3.transform.rotation.y =  -0.66423
+        t3.transform.rotation.z =  -0.66423
+        t3.transform.rotation.w =  0.242465
 
 
         self.br.sendTransform([t1,t2,t3])
